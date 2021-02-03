6327e41fb13a8300056de3ba4adba50245b36b88
Ebenezer Don <ebenezerdonu@gmail.com>
2021-02-03 02:55:00 +0100

    add itemHasBeenPurchased property

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ const App = () => {
     event.preventDefault()
     const name = document.querySelector('.item-name').value
     const price = document.querySelector('.item-price').value
-    db.items.add({ name, price })
+    db.items.add({ name, price, itemHasBeenPurchased: false })
   }
 
   const removeItemFromDb = async id => {
